4ce468434a6b8c1cb8de68ae8033eeedfa841e5d
Marciele Adivincula <45802716+marcieleadivincula@users.noreply.github.com>
2023-04-10 09:38:17 -0400

    Update conditional2.py

--- aulaPraticaLogica/conditional2.py
+++ aulaPraticaLogica/conditional2.py
@@ -18,27 +18,28 @@ print(min(34, 29, 31) < 30)
 # Exercícios de condicionais simples
 # A) Se idade é maior que 60 escreva: 'Você tem direitos aos benefícios'
 idade = int(input('Digite a sua idade: '))
-if(idade > 60):
+if idade > 60:
     print('Você tem direitos aos benefícios')
 # B) Se dano é maior do que 10 e escudo é igual a 0, escreva: 'Você está morto!'
 dano = int(input('Digite o valor do dano: '))
-escudo =  int(input('Digite o valor do escudo: '))
-if(dano > 10 and escudo == 0):
+escudo = int(input('Digite o valor do escudo: '))
+if dano > 10 and escudo == 0:
     print('Você está morto!')
 # C) Se pelo menos uma das variáveis booleanas norte, sul, leste e oeste resultarem em True, escreva: 'Você escapou!'
-if(norte == True or sul == True or leste == True or oeste == True):
+if norte is True or sul is True or leste is True or oeste is True:
     # podemos simplificar a expressão acima assim: if(norte or sul or leste or oeste):
     print('Você escapou!')
 
 # Exercícios de condicionais composta
-# A) Se ano é divisível por 4, escreva: 'Pode ser um ano bissexto'. Caso contrário, escreva: 'Definitivamente, não é um ano bissexto'
-if(ano % 4 == 0):
+# A) Se ano é divisível por 4, escreva: 'Pode ser um ano bissexto'. Caso contrário,
+# escreva: 'Definitivamente, não é um ano bissexto'
+if ano % 4 == 0:
     print('Pode ser um ano bissexto')
 else:
     print('Definitivamente, não é um ano bissexto')
-# B)Se ambas as variáveis booleanas cima e baixo forem True, escreva: 'Decida-se!', caso contrário, escreva: 'Você escolheu um caminho!'
-if(cima == True and baixo == True):
+# B)Se ambas as variáveis booleanas cima e baixo forem True, escreva: 'Decida-se!',
+# caso contrário, escreva: 'Você escolheu um caminho!'
+if cima is True and baixo is True:
     print('Decida-se!')
 else:
     print('Você escolheu um caminho!')
-
