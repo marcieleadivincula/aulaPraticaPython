19a5ac34bfd54b194255a13f900119cc19cdc265
Marciele Adivincula <45802716+marcieleadivincula@users.noreply.github.com>
2023-04-10 09:38:38 -0400

    Create APOL2.py

---
+++ aulaPraticaLogica/APOL2.py
@@ -0,0 +1,37 @@
+# i = 88
+# while i >= 0:
+#     print(i)
+#     i -= 4
+
+# for i in range(88, -1, -4):
+#     print(i)
+
+# x = 10
+# while x <= 100:
+#     print(x)
+#     x += 1
+
+# for i in range(7, 26, 3):
+#     print(i)
+
+cont = 5
+soma = 0
+while cont <= 25:
+    soma = soma + cont
+    # print('SOMA: {} - CONT: {}'.format(soma, cont))
+    cont = cont + 5
+    print('SOMA: {} - CONT: {}'.format(soma, cont))
+print(soma)
+
+# for i in range(10, 20):
+#     for j in range(10, 20, 2):
+#         print('{} + {} = {}'.format(i, j, i + j))
+#
+# for i in range(100, 1000, 10):
+#     print(i)
+
+# i = 100
+# while i < 1000:
+#     print(i)
+#     i += 10
+
